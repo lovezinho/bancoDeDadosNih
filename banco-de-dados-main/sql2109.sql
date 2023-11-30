8fa817fa932a8b9fcda9325ff1b962c234092dcb
nicole luana <126881823+lovezinho@users.noreply.github.com>
2023-11-30 16:57:19 -0300

    Add files via upload

---
+++ banco-de-dados-main/sql2109.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE func2d;
+CREATE TABLE `func2d`.`funcionarios` (`id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(255) NOT NULL , `cpf` VARCHAR(11) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
